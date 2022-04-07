fd4988e7ef427990c9c00366c4d1e0bfcb709e72
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-04-07 11:41:56 -0400

    Add clean function

--- root/scripts/scrape.sh
+++ root/scripts/scrape.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/with-contenv bash
 for folder in $(ls /output);
-do 
+do
+	# Scrape from screenscraper
+	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend
+	# Save scraped data to output folder
+	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend
+	# Remove skipped roms
+	cat /root/.skyscraper/skipped-$folder-cache.txt | while read LINE;
+	do 
+		rm "$LINE"
+	done
+	# Rebuild gamelist to clean
 	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend
 	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend
 done
+# set permissions
 find /output -type d -exec chmod 777 {} \;
 find /output -type f -exec chmod 666 {} \;
 exit $?
