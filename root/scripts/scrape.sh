bff736848f24956978d71b8be56fa3d3bbfed3a1
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-04-07 09:59:26 -0400

    add unattend flag

--- root/scripts/scrape.sh
+++ root/scripts/scrape.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/with-contenv bash
 for folder in $(ls /output);
 do 
-	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos
-	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos
+	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend
+	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend
 done
 find /output -type d -exec chmod 777 {} \;
 find /output -type f -exec chmod 666 {} \;
