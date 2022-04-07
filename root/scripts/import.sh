2f6e4b6def23e04635f7736065497c58b8835d17
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-04-07 11:42:55 -0400

    Update import.sh

--- root/scripts/import.sh
+++ root/scripts/import.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/with-contenv bash
+# Process ROMs with hascheevos
 find /import -type f | while read LINE;
 do
 	sudo -u abc bash /usr/local/hascheevos/bin/hascheevos.sh --copy-roms-to /output  "$LINE"
 done
+# Set permissions
 find /output -type d -exec chmod 777 {} \;
 find /output -type f -exec chmod 666 {} \;
 exit $?
