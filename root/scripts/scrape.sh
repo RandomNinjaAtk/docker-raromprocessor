a3dab81073934a912861508fb2127d79c89ccd4b
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-04-07 09:55:41 -0400

    include videos, separate platform cache data

--- root/scripts/scrape.sh
+++ root/scripts/scrape.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/with-contenv bash
 for folder in $(ls /output);
 do 
-	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache -s screenscraper -i /output/$folder --flags relative
-	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache -i /output/$folder --flags relative
+	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos
+	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos
 done
 find /output -type d -exec chmod 777 {} \;
 find /output -type f -exec chmod 666 {} \;
