6c96c1e29b53d1b3ecc8a9116a19ccb9b7686fee
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-04-08 09:39:02 -0400

    Add unpack flag for increased accuracy during first scan

--- root/scripts/scrape.sh
+++ root/scripts/scrape.sh
@@ -2,7 +2,7 @@
 for folder in $(ls /output);
 do
 	# Scrape from screenscraper
-	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend,nobrackets
+	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend,nobrackets,unpack
 	# Save scraped data to output folder
 	Skyscraper -f emulationstation -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend,nobrackets
 	# Remove skipped roms
