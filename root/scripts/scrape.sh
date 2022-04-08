c74905e6df1c7c379daf84751d7647eb25027789
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-04-07 21:48:45 -0400

    Move empty directory cleanup

--- root/scripts/scrape.sh
+++ root/scripts/scrape.sh
@@ -13,9 +13,9 @@ do
 	# Rebuild gamelist to clean
 	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend,nobrackets
 	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend,nobrackets
-done
 	# remove empty directories
 	find /input -type d -empty -exec rm -rf {} \; &>/dev/null
+done
 # set permissions
 find /output -type d -exec chmod 777 {} \;
 find /output -type f -exec chmod 666 {} \;
