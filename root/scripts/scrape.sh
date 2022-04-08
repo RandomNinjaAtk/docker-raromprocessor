b96a2fde424527ca6c0ec6f0d7e21a28d12f8ce8
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-04-07 21:51:03 -0400

    avoid /input folder deletion (bugfix)

--- root/scripts/scrape.sh
+++ root/scripts/scrape.sh
@@ -14,7 +14,7 @@ do
 	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend,nobrackets
 	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend,nobrackets
 	# remove empty directories
-	find /input -type d -empty -exec rm -rf {} \; &>/dev/null
+	find /input -mindepth 1 -type d -empty -exec rm -rf {} \; &>/dev/null
 done
 # set permissions
 find /output -type d -exec chmod 777 {} \;
