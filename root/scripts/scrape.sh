dcc0b950384685a0c079f61713c3eaf334cbf65d
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-04-07 21:47:37 -0400

    Add nobrackets flag, cleanup empty directories after successful backup

--- root/scripts/scrape.sh
+++ root/scripts/scrape.sh
@@ -2,18 +2,20 @@
 for folder in $(ls /output);
 do
 	# Scrape from screenscraper
-	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend
+	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend,nobrackets
 	# Save scraped data to output folder
-	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend
+	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend,nobrackets
 	# Remove skipped roms
 	cat /root/.skyscraper/skipped-$folder-cache.txt | while read LINE;
 	do 
 		rm "$LINE"
 	done
 	# Rebuild gamelist to clean
-	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend
-	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend
+	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend,nobrackets
+	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend,nobrackets
 done
+	# remove empty directories
+	find /input -type d -empty -exec rm -rf {} \; &>/dev/null
 # set permissions
 find /output -type d -exec chmod 777 {} \;
 find /output -type f -exec chmod 666 {} \;
