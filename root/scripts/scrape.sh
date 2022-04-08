51df83782a6d65e3ff23da70bf97f7505f207a85
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-04-08 08:21:33 -0400

    Add DeDupe process

--- root/scripts/scrape.sh
+++ root/scripts/scrape.sh
@@ -15,8 +15,105 @@ do
 	# Rebuild gamelist to clean
 	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend,nobrackets
 	Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend,nobrackets
-	# remove empty directories
-	find /input -mindepth 1 -type d -empty -exec rm -rf {} \; &>/dev/null
+	
+	if [ "$DeDupe" = "true" ]; then
+		GameListData="$(cat /output/$folder/gamelist.xml | xq .gameList[] | jq .[])"
+		OLDIFS="$IFS"
+		IFS=$'\n'
+		GameNames=($(echo "$GameListData" | jq -r ".name" | sort -u))
+		IFS="$OLDIFS"		
+		for Name in ${!GameNames[@]}; do
+			GameName="${GameNames[$Name]}"
+			RomCount=""
+			RomCount=$(echo $GameListData | jq -r .name | grep "^$GameName$" | wc -l)
+			if [ $RomCount -gt 1 ]; then
+				echo "Dupes found for: $GameName"
+				Files=$(echo "$GameListData" | jq -r "select(.name==\"$GameName\") | .path")
+				OLDIFS="$IFS"
+				IFS=$'\n'
+				FileNames=($(echo "$GameListData" | jq -r "select(.name==\"$GameName\") | .path"))
+				IFS="$OLDIFS"
+				echo "$Files"
+				UsaRom=false
+				EuropeRom=false
+				JapanRom=false
+				if echo "$Files" | grep "USA" | read; then
+					echo "USA ROM Found"
+					echo "Delete Non USA ROM"
+					UsaRom=true
+				elif echo "$Files" | grep "Europe" | read; then
+					echo "Europe ROM Found"
+					echo "Delete Non Europe ROM"
+					EuropeRom=true
+				elif  echo "$Files" | grep "Japan" | read; then
+					echo "Japan ROM Found"
+					echo "Delete Non Japan ROM"
+					JapanRom=true			
+				fi
+
+
+				for File in ${!FileNames[@]}; do
+					FileName="${FileNames[$File]}"
+					FileName="${FileName:2}"
+					if [ $UsaRom = true ]; then
+						if echo "$FileName" | grep -v "USA" | read; then
+							echo "Delete /output/$folder/$FileName"
+							if [ -f "/output/$folder/$FileName" ]; then
+								rm "/output/$folder/$FileName"
+							fi
+						fi
+					fi
+					if [ $EuropeRom = true ]; then
+						if echo "$FileName" | grep -v "Europe" | read; then
+							echo "Delete /output/$folder/$FileName"
+							if [ -f "/output/$folder/$FileName" ]; then
+								rm "/output/$folder/$FileName"
+							fi
+						fi
+					fi
+					if [ $JapanRom = true ]; then
+						if echo "$FileName" | grep -v "Japan" | read; then
+							echo "Delete /output/$folder/$FileName"
+							if [ -f "/output/$folder/$FileName" ]; then
+								rm "/output/$folder/$FileName"
+							fi
+						fi
+					fi
+
+
+				done
+
+				for File in ${!FileNames[@]}; do
+					FileName="${FileNames[$File]}"
+					FileName="${FileName:2}"
+					FileNameLength=$(echo $FileName | wc -m)
+					if [ -f "/output/$folder/$FileName" ]; then
+						echo "FileNameLength $FileNameLength :: $FileName"
+						for ComparisonFile in ${!FileNames[@]}; do
+							ComparisonFileName="${FileNames[$ComparisonFile]}"
+							ComparisonFileName="${ComparisonFileName:2}"
+							ComparisonFileNameLength=$(echo $ComparisonFileName | wc -m)
+							echo "ComparisonFileNameLength $ComparisonFileNameLength :: $ComparisonFileName"
+							if [ $FileNameLength -gt $ComparisonFileNameLength ]; then
+								if [ -f "/output/$folder/$ComparisonFileName" ]; then
+									echo "DELETE :: /output/$folder/$FileName"
+									rm "/output/$folder/$FileName"
+								fi
+							fi
+						done
+					fi
+				done	
+
+			else
+				echo "No dupes found for: $GameName"
+			fi
+		done
+		# Rebuild gamelist to clean
+		Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -s screenscraper -i /output/$folder --flags relative,videos,unattend,nobrackets
+		Skyscraper -f emulationstation -u $ScreenscraperUsername:$ScreenscraperPassword -p $folder -d /cache/$folder -i /output/$folder --flags relative,videos,unattend,nobrackets
+	else
+		echo "DeDuping process disabled..."
+	fi
 done
 # set permissions
 find /output -type d -exec chmod 777 {} \;
