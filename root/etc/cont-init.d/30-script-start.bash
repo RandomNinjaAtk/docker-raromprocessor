6754cef9440a6c6051075378ac38ebeb55861418
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-04-08 21:27:11 -0400

    Create ROM input folders

--- root/etc/cont-init.d/30-script-start.bash
+++ root/etc/cont-init.d/30-script-start.bash
@@ -1,4 +1,56 @@
 #!/usr/bin/with-contenv bash
+echo "Setting Up ROM /ipnut directories"
+echo "Created: /input/megadrive"
+echo "Created: /input/n64"
+echo "Created: /input/snes"
+echo "Created: /input/gb"
+echo "Created: /input/gba"
+echo "Created: /input/gbc"
+echo "Created: /input/nes"
+echo "Created: /input/pcengine"
+echo "Created: /input/segacd"
+echo "Created: /input/sega32x"
+echo "Created: /input/mastersystem"
+echo "Created: /input/psx"
+echo "Created: /input/atarilynx"
+echo "Created: /input/ngp"
+echo "Created: /input/gamegear"
+echo "Created: /input/atarijaguar"
+echo "Created: /input/nds"
+echo "Created: /input/pokemini"
+echo "Created: /input/atari2600"
+echo "Created: /input/arcade"
+echo "Created: /input/virtualboy"
+echo "Created: /input/sg-1000"
+echo "Created: /input/coleco"
+echo "Created: /input/atari7800"
+echo "Created: /input/wonderswan"
+mkdir -p /input/megadrive
+mkdir -p /input/n64
+mkdir -p /input/snes
+mkdir -p /input/gb
+mkdir -p /input/gba
+mkdir -p /input/gbc
+mkdir -p /input/nes
+mkdir -p /input/pcengine
+mkdir -p /input/segacd
+mkdir -p /input/sega32x
+mkdir -p /input/mastersystem
+mkdir -p /input/psx
+mkdir -p /input/atarilynx
+mkdir -p /input/ngp
+mkdir -p /input/gamegear
+mkdir -p /input/atarijaguar
+mkdir -p /input/nds
+mkdir -p /input/pokemini
+mkdir -p /input/atari2600
+mkdir -p /input/arcade
+mkdir -p /input/virtualboy
+mkdir -p /input/sg-1000
+mkdir -p /input/coleco
+mkdir -p /input/atari7800
+mkdir -p /input/wonderswan
+
 
 if [ "$AutoStart" = "true" ]; then
 	echo "Automatic Start Enabled, starting..."
