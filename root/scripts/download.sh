02d81ee52b3b6702c60c98c1ec2a08b6992727a8
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-04-10 13:18:32 -0400

    Sega CD Archive.org

--- root/scripts/download.sh
+++ root/scripts/download.sh
@@ -66,6 +66,7 @@ for folder in $(ls /input); do
 		ConsoleId=9
 		ConsoleName="Sega CD"
 		ConsoleDirectory="segacd"
+		ArchiveUrl="https://archive.org/compress/SEGACD_CHD_PLUS"
 	fi
 
 	if echo "$folder" | grep "^sega32x" | read; then
