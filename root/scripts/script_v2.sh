6e76b1418ed8ae9b6d6fe54f987e902cb75f0fcd
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-20 16:17:33 -0500

    wip

--- root/scripts/script_v2.sh
+++ root/scripts/script_v2.sh
@@ -22,22 +22,24 @@ DownloadFile () {
   # $1 = URL
   # $2 = Output Folder/file
   # #3 = Number of concurrent connections to use
+  Log "Downloading :: $1"
   axel -q -n $3 --output="$2" "$1"
+  chmod 666 "$2"
 }
 
 DownloadFileVerification () {
   case "$1" in
     *.zip|*.ZIP)
-      verify="$(unzip -t "$DownloadOutput" &>/dev/null; echo $?)"
+      verify="$(unzip -t "$1" &>/dev/null; echo $?)"
       ;;
     *.rar|*.RAR)
-      verify="$(unrar t "$DownloadOutput" &>/dev/null; echo $?)"
+      verify="$(unrar t "$1" &>/dev/null; echo $?)"
       ;;
     *.7z|*.7Z)
-      verify="$(7z t "$DownloadOutput" &>/dev/null; echo $?)"
+      verify="$(7z t "$1" &>/dev/null; echo $?)"
       ;;
     *.chd|*.CHD)
-      verify="$(chdman verify -i "$DownloadOutput" &>/dev/null; echo $?)"
+      verify="$(chdman verify -i "$1" &>/dev/null; echo $?)"
       ;;
     *.iso|*.ISO)
       verify="0"
@@ -45,8 +47,10 @@ DownloadFileVerification () {
   esac
   
   if [ "$verify" != "0" ]; then
-    log "$ConsoleName :: $currentsubprocessid of $DlCount :: $romFile :: Download Failed!"
-    rm -rf "$1"
+    Log "$1 :: ERROR :: Failed Verification!"
+    rm "$1"
+  else
+    Log "$1 :: Download Verified!"
   fi
 }
 
@@ -56,12 +60,15 @@ UncompressFile () {
   Log "Uncompressing \"$1\" to \"$2\""
   case "$1" in
     *.zip|*.ZIP)
-      unzip -o -d "$1" "$2" >/dev/null
+      Log "Zip file detected!"
+      unzip -o -d "$2" "$1" >/dev/null
       ;;
     *.rar|*.RAR)
+      Log "Rar file detected!"
       unrar x "$1" "$2" &>/dev/null
       ;;
     *.7z|*.7Z)
+      Log "7z file detected!"
       7z x "$1" "$2" &>/dev/null
       ;;
   esac
@@ -70,4 +77,9 @@ UncompressFile () {
 
 ######### PROCESS
 
+url="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Nintendo%20-%20Nintendo%20Entertainment%20System.zip/Nintendo%20-%20Nintendo%20Entertainment%20System%2Fnes%2FLegend%20of%20Zelda%2C%20The%20%28USA%29%20%28Rev%201%29.zip"
+rm "/config/temp.zip"
+DownloadFile "$url" "/config/temp.zip" "10"
+DownloadFileVerification "/config/temp.zip"
+UncompressFile "/config/temp.zip" "/config"
 exit
