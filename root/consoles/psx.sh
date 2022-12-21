dfc124c57d63d6d6a591692f8d1ebda20f43d039
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-21 16:18:13 -0500

    Update psx.sh

--- root/consoles/psx.sh
+++ root/consoles/psx.sh
@@ -9,13 +9,13 @@ compressRom="false"
 if [ -f templist ]; then
     rm templist
 fi
-archiveUrl="$(curl -s "https://archive.org/download/chd_psx/CHD-PSX-USA/" | grep ".chd" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed 's|^|https://archive.org/download/chd_psx/CHD-PSX-USA/|')"
+archiveUrl="$(curl -s "https://archive.org/download/chd_psx/CHD-PSX-USA/" | grep -i "\.chd" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed 's|^|https://archive.org/download/chd_psx/CHD-PSX-USA/|')"
 echo "$archiveUrl" >> templist
-archiveUrl="$(curl -s "https://archive.org/download/chd_psx_eur/CHD-PSX-EUR/" | grep ".chd" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed 's|^|https://archive.org/download/chd_psx_eur/CHD-PSX-EUR/|')"
+archiveUrl="$(curl -s "https://archive.org/download/chd_psx_eur/CHD-PSX-EUR/" | grep -i "\.chd" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed 's|^|https://archive.org/download/chd_psx_eur/CHD-PSX-EUR/|')"
 echo "$archiveUrl" >> templist
-archiveUrl="$(curl -s "https://archive.org/download/chd_psx_jap/CHD-PSX-JAP/" | grep ".chd" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed 's|^|https://archive.org/download/chd_psx_jap/CHD-PSX-JAP/|')"
+archiveUrl="$(curl -s "https://archive.org/download/chd_psx_jap/CHD-PSX-JAP/" | grep -i "\.chd" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed 's|^|https://archive.org/download/chd_psx_jap/CHD-PSX-JAP/|')"
 echo "$archiveUrl" >> templist
-archiveUrl="$(curl -s "https://archive.org/download/chd_psx_jap_p2/CHD-PSX-JAP/" | grep ".chd" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed 's|^|https://archive.org/download/chd_psx_jap_p2/CHD-PSX-JAP/|')"
+archiveUrl="$(curl -s "https://archive.org/download/chd_psx_jap_p2/CHD-PSX-JAP/" | grep -i "\.chd" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed 's|^|https://archive.org/download/chd_psx_jap_p2/CHD-PSX-JAP/|')"
 echo "$archiveUrl" >> templist
 archiveUrl=$(cat templist)
 if [ -f templist ]; then
