fe0784c5d1e4d5ad626a7a0b2c2bcfda30239749
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-21 11:45:12 -0500

    Update nes.sh

--- root/consoles/nes.sh
+++ root/consoles/nes.sh
@@ -4,4 +4,7 @@ consoleRomFileExt=".nes, .unif, .unf, .zip, .7z"
 raConsoleId="7"
 uncompressRom="true"
 compressRom="true"
+
+# Create URL List
 archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Nintendo%20-%20Nintendo%20Entertainment%20System.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
