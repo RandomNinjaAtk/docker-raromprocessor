b9100182533b3192f96d275170d2c2dd38d1d688
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-21 11:45:42 -0500

    Update snes.sh

--- root/consoles/snes.sh
+++ root/consoles/snes.sh
@@ -4,4 +4,7 @@ consoleRomFileExt=".smc, .fig, .sfc, .gd3, .gd7, .dx2, .bsx, .swc, .zip, .7z"
 raConsoleId="3"
 uncompressRom="true"
 compressRom="true"
+
+# Create URL List
 archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Nintendo%20-%20Super%20Nintendo%20Entertainment%20System.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
