0991dd7ce7f23472d8a4c498bd4c63466637e580
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-21 14:47:52 -0500

    Create sega32x.sh

---
+++ root/consoles/sega32x.sh
@@ -0,0 +1,10 @@
+consoleName="Sega 32X"
+consoleFolder="sega32x"
+consoleRomFileExt=".32x, .smd, .bin, .md, .zip, .7z"
+raConsoleId="10"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Sega%20-%2032X.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
