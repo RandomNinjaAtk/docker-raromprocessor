646e1b728a4ebad7f0aa0604981c3b083254913f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-21 16:22:36 -0500

    Create gbc.sh

---
+++ root/consoles/gbc.sh
@@ -0,0 +1,10 @@
+consoleName="GameBoy Color"
+consoleFolder="gbc"
+consoleRomFileExt=".gbc, .zip, .7z"
+raConsoleId="6"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Nintendo - Game Boy Color.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
