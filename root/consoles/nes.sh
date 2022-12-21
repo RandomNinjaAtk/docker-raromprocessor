d190133e4e250270d7cd27e1b9c2b0906ed47449
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-21 14:03:56 -0500

    Update nes.sh

--- root/consoles/nes.sh
+++ root/consoles/nes.sh
@@ -4,6 +4,7 @@ consoleRomFileExt=".nes, .unif, .unf, .zip, .7z"
 raConsoleId="7"
 uncompressRom="true"
 compressRom="true"
+raGameList="$(wget -qO- "https://retroachievements.org/API/API_GetGameList.php?z=${raUsername}&y=${raWebApiKey}&i=$raConsoleId")"
 
 # Create URL List
 archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Nintendo%20-%20Nintendo%20Entertainment%20System.zip/"
