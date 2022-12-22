ebe42b75a259e7726a4b507995100209c22c6022
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 11:24:41 -0500

    Create ngpc.sh

---
+++ root/consoles/ngpc.sh
@@ -0,0 +1,10 @@
+consoleName="SNK Neo Geo Pocket Color"
+consoleFolder="ngpc"
+consoleRomFileExt=".ngc, .zip, .7z"
+raConsoleId="14"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/SNK - Neo Geo Pocket Color.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
