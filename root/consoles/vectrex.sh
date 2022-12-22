a767c8d3df2ec36c949d5dbf53403e125c8c65e4
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 10:17:45 -0500

    Create vectrex.sh

---
+++ root/consoles/vectrex.sh
@@ -0,0 +1,10 @@
+consoleName="Vectrex"
+consoleFolder="vectrex"
+consoleRomFileExt=".bin, .gam, .vec, .zip, .7z"
+raConsoleId="46"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/GCE - Vectrex.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
