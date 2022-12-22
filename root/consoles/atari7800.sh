ecd3c547102932c7bac77f49ab58280ccd120b3d
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 09:28:03 -0500

    Create atari7800.sh

---
+++ root/consoles/atari7800.sh
@@ -0,0 +1,10 @@
+consoleName="Atari 7800"
+consoleFolder="atari7800"
+consoleRomFileExt=".a78, .bin, .zip, .7z"
+raConsoleId="51"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Atari - 7800.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
