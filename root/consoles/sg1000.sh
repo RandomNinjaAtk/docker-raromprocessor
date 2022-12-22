e17d7352065d3b570c0cd6f6e05003c22ec59409
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 09:22:43 -0500

    Create sg1000.sh

---
+++ root/consoles/sg1000.sh
@@ -0,0 +1,10 @@
+consoleName="SG-1000"
+consoleFolder="sg1000"
+consoleRomFileExt=".bin, .sg, .zip, .7z"
+raConsoleId="33"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Sega - SG-1000.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
