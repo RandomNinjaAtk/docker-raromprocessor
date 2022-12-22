b6a2c2087acc08cf3e3612d2255d981ef08bb62f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 10:39:16 -0500

    Create jaguar.sh

---
+++ root/consoles/jaguar.sh
@@ -0,0 +1,10 @@
+consoleName="Atari Jaguar"
+consoleFolder="jaguar"
+consoleRomFileExt=".j64, .jag, .zip, .7z"
+raConsoleId="17"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Atari - Jaguar.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
