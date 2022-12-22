ae8c0ee27162773aa5d637bd4d320a049c87bba7
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 08:57:48 -0500

    Create virtualboy.sh

---
+++ root/consoles/virtualboy.sh
@@ -0,0 +1,10 @@
+consoleName="VirtualBoy"
+consoleFolder="virtualboy"
+consoleRomFileExt=".vb, .zip, .7z"
+raConsoleId="28"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Nintendo - Virtual Boy.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
