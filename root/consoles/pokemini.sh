929e1756ff16f9805a23de5fa67f0ec6d082e1a4
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 08:55:12 -0500

    Create pokemini.sh

---
+++ root/consoles/pokemini.sh
@@ -0,0 +1,10 @@
+consoleName="Pokemon Mini"
+consoleFolder="pokemini"
+consoleRomFileExt=".min, .zip, .7z"
+raConsoleId="24"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Nintendo - Pokemon Mini.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
