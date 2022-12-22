a62e4c7060ae683cdf05bb59422aba22726705f1
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 10:58:09 -0500

    Create n64.sh

---
+++ root/consoles/n64.sh
@@ -0,0 +1,10 @@
+consoleName="Nintendo 64"
+consoleFolder="n64"
+consoleRomFileExt=".z64 .n64 .v64 .zip .7z"
+raConsoleId="2"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Nintendo - Nintendo 64.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
