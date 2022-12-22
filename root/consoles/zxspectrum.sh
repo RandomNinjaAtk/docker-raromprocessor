cf40146298b832b605190705c84fd90d819d5554
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 12:07:44 -0500

    Create zxspectrum.sh

---
+++ root/consoles/zxspectrum.sh
@@ -0,0 +1,10 @@
+consoleName="ZXSpectrum"
+consoleFolder="zxspectrum"
+consoleRomFileExt=".tzx, .tap, .z80, .rzx, .scl, .trd, .zip, .7"
+raConsoleId="59"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Sinclair%20-%20ZX%20Spectrum.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
