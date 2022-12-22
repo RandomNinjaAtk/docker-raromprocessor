0096188fcf1aab3df5cad90969291622d2b89888
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 08:52:41 -0500

    Create gamegear.sh

---
+++ root/consoles/gamegear.sh
@@ -0,0 +1,10 @@
+consoleName="Game Gear"
+consoleFolder="gamegear"
+consoleRomFileExt=".bin, .gg, .zip, .7z"
+raConsoleId="15"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Sega - Game Gear.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
