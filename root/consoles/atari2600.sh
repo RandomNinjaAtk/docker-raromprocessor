ccb3ac1a204fa5074267611f9ccb1a82fe10c95a
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 10:24:16 -0500

    Create atari2600.sh

---
+++ root/consoles/atari2600.sh
@@ -0,0 +1,10 @@
+consoleName="Atari 2600"
+consoleFolder="atari2600"
+consoleRomFileExt=".a26, .bin, .zip, .7z"
+raConsoleId="25"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Atari - 2600.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
