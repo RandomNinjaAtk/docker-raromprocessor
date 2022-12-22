62dc60eadd998a2cd09034154406ba36217114f4
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 09:54:04 -0500

    Create wswan.sh

---
+++ root/consoles/wswan.sh
@@ -0,0 +1,10 @@
+consoleName="WonderSwan"
+consoleFolder="wswan"
+consoleRomFileExt=".ws, .zip, .7z"
+raConsoleId="53"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Bandai - WonderSwan.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
