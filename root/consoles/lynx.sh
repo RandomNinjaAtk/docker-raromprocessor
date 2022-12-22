2e674b515645f03077c9057547e696898d3af79f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-22 10:55:41 -0500

    Create lynx.sh

---
+++ root/consoles/lynx.sh
@@ -0,0 +1,10 @@
+consoleName="Atari Lynx"
+consoleFolder="lynx"
+consoleRomFileExt=".lnx, .zip, .7z"
+raConsoleId="13"
+uncompressRom="true"
+compressRom="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Atari - Lynx.zip/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
