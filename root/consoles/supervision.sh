fbdcd8ae4c21e56b6e56c146aa769e821f17b252
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-23 13:44:28 -0500

    Create supervision.sh

---
+++ root/consoles/supervision.sh
@@ -0,0 +1,10 @@
+consoleName="Watara Supervision"
+consoleFolder="supervision"
+consoleRomFileExt=".sv, .zip, .7z"
+raConsoleId="63"
+uncompressRom="false"
+compressRom="false"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/WataraSupervision_201809/ROMS.zip//"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
