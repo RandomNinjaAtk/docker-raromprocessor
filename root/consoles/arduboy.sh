dda56d89f2871ea253dcf2a09f217f1f53d401b7
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-23 13:11:35 -0500

    Create arduboy.sh

---
+++ root/consoles/arduboy.sh
@@ -0,0 +1,10 @@
+consoleName="Arduboy"
+consoleFolder="arduboy"
+consoleRomFileExt=".hex, .zip, .7z"
+raConsoleId="71"
+uncompressRom="false"
+compressRom="false"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/ArduboyRoms/Arduboy/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.hex" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
