a98658f772964cefb0238e2e58ee71f898ab921a
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-23 10:05:36 -0500

    Update nds.sh

--- root/consoles/nds.sh
+++ root/consoles/nds.sh
@@ -7,4 +7,4 @@ compressRom="true"
 
 # Create URL List
 archiveContentsUrl="https://archive.org/download/noIntroNintendoDsDecrypted2020Jan20/"
-archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.zip" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed 's|^|https://archive.org/download/noIntroNintendoDsDecrypted2020Jan20/|')"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.zip" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
