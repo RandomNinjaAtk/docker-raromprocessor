a76f34f8c5bbaa46e13a8eb7647011bfd972ca73
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-23 10:07:40 -0500

    Create saturn.sh

---
+++ root/consoles/saturn.sh
@@ -0,0 +1,10 @@
+consoleName="Sega Saturn"
+consoleFolder="saturn"
+consoleRomFileExt=".cue, .ccd, .m3u, .chd, .iso, .zip"
+raConsoleId="39"
+uncompressRom="false"
+compressRom="false"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/chd_saturn/CHD-Saturn/USA/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.chd" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
