6940aa8c8ee730008af60dccaa78b86af4adbb47
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-23 11:37:44 -0500

    Update psp.sh

--- root/consoles/psp.sh
+++ root/consoles/psp.sh
@@ -6,16 +6,6 @@ uncompressRom="false"
 compressRom="false"
 
 # Create URL List
-if [ -f templist ]; then
-    rm templist
-fi
-archiveContentsUrl="https://archive.org/download/psp_20220507/"
-archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.cso" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
-echo "$archiveUrl" >> templist
-archiveContentsUrl="https://archive.org/download/psp_20220507_2"
-archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.cso" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
-echo "$archiveUrl" >> templist
-archiveUrl=$(cat templist)
-if [ -f templist ]; then
-    rm templist
-fi
+
+archiveContentsUrl="https://archive.org/download/psp-redump-roms321.com/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.7z" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
