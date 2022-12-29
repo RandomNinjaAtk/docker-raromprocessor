7e1fef86a75aba7630c11b3deff53ee3dd5792f3
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-29 09:13:19 -0500

    Update pcfx.sh

--- root/consoles/pcfx.sh
+++ root/consoles/pcfx.sh
@@ -6,5 +6,5 @@ uncompressRom="false"
 compressRom="false"
 
 # Create URL List
-archiveContentsUrl="https://archive.org/download/PCFX_Arquivista/"
-archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.zip" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
+archiveContentsUrl="https://archive.org/download/PCFXCHD_Arquivista/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.chd" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
