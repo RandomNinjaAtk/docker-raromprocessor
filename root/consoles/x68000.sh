c5a151e50b7ab74295b31428793aa456728ac600
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-30 16:30:21 -0500

    Update x68000.sh

--- root/consoles/x68000.sh
+++ root/consoles/x68000.sh
@@ -4,6 +4,8 @@ consoleRomFileExt=".dim, .img, .d88, .88d, .hdm, .dup, .2hd, .xdf, .hdf, .cmd, .
 raConsoleId="52"
 uncompressRom="true"
 compressRom="true"
+skyscraperPlatform="x68000"
+downloadAll="true"
 
 # Create URL List
 archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Sharp%20-%20X68000.zip/"
