85719ce4874e476e093d49188f4ae8afa2f718df
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-30 16:15:53 -0500

    Update intellivision.sh

--- root/consoles/intellivision.sh
+++ root/consoles/intellivision.sh
@@ -4,6 +4,8 @@ consoleRomFileExt=".int, .bin, .rom, .zip, .7z"
 raConsoleId="45"
 uncompressRom="true"
 compressRom="true"
+skyscraperPlatform="intellivision"
+downloadAll="true"
 
 # Create URL List
 archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Mattel - Intellivision.zip/"
