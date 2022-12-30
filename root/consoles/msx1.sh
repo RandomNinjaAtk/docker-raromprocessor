48fe0c30175935d9eff96ebe97cff8583984779f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-30 16:18:25 -0500

    Update msx1.sh

--- root/consoles/msx1.sh
+++ root/consoles/msx1.sh
@@ -4,6 +4,8 @@ consoleRomFileExt=".dsk, .mx1, .rom, .zip, .7z, .cas, .m3u"
 raConsoleId="29"
 uncompressRom="true"
 compressRom="true"
+skyscraperPlatform="msx"
+downloadAll="true"
 
 # Create URL List
 archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Microsoft - MSX.zip/"
