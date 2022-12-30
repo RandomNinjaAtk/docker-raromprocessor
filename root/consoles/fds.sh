c87a88a0b0318d2022e630302c183e323f591b3c
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-12-29 20:35:17 -0500

    Create fds.sh

---
+++ root/consoles/fds.sh
@@ -0,0 +1,11 @@
+consoleName="Family Computer Disk System"
+consoleFolder="fds"
+consoleRomFileExt=".fds, .zip, .7z"
+raConsoleId="7"
+uncompressRom="true"
+compressRom="true"
+downloadAll="true"
+
+# Create URL List
+archiveContentsUrl="https://archive.org/download/nointro.fds/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.7z" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
