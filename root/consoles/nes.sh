3fab81c120470afaab337d79dfca8c7a9797ecd0
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-01-01 14:19:51 -0500

    New set

--- root/consoles/nes.sh
+++ root/consoles/nes.sh
@@ -3,10 +3,10 @@ consoleFolder="nes"
 consoleRomFileExt=".nes, .unif, .unf, .zip, .7z"
 raConsoleId="7"
 uncompressRom="true"
-compressRom="true"
+compressRom="false"
 skyscraperPlatform="nes"
 downloadAll="true"
 
 # Create URL List
-archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Nintendo%20-%20Nintendo%20Entertainment%20System.zip/"
-archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
+archiveContentsUrl="https://archive.org/download/nointro.nes-headered/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.7z" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
