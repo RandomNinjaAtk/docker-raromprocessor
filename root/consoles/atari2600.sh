f0779cfacbc3b762ccfed2b194d55a90a93e4de7
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-01-01 19:56:03 -0500

    change to no-intro

--- root/consoles/atari2600.sh
+++ root/consoles/atari2600.sh
@@ -3,10 +3,10 @@ consoleFolder="atari2600"
 consoleRomFileExt=".a26, .bin, .zip, .7z"
 raConsoleId="25"
 uncompressRom="true"
-compressRom="true"
+compressRom="false"
 skyscraperPlatform="atari2600"
 downloadAll="true"
 
 # Create URL List
-archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Atari - 2600.zip/"
-archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
+archiveContentsUrl="https://archive.org/download/nointro.atari-2600/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.7z" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
