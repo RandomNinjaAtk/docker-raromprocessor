69657615da1ea821c4300514c3ed24e7665f3625
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-01-01 19:45:11 -0500

    change to gba no-intro set

--- root/consoles/gba.sh
+++ root/consoles/gba.sh
@@ -3,10 +3,10 @@ consoleFolder="gba"
 consoleRomFileExt=".gba, .zip, .7z"
 raConsoleId="5"
 uncompressRom="true"
-compressRom="true"
+compressRom="false"
 skyscraperPlatform="gba"
 downloadAll="true"
 
 # Create URL List
-archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Nintendo - Game Boy Advance.zip/"
-archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
+archiveContentsUrl="https://archive.org/download/nointro.gba/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.7z" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
