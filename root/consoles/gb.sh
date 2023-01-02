1651d0ad6730a7dceb398c5fb7b8c14f15775c3c
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-01-01 19:43:04 -0500

    Update gb.sh

--- root/consoles/gb.sh
+++ root/consoles/gb.sh
@@ -3,10 +3,10 @@ consoleFolder="gb"
 consoleRomFileExt=".gb, .zip, .7z"
 raConsoleId="4"
 uncompressRom="true"
-compressRom="true"
+compressRom="false"
 skyscraperPlatform="gb"
-downloadAll="false"
+downloadAll="true"
 
 # Create URL List
-=archiveContentsUrl="https://archive.org/download/nointro.gb/"
+archiveContentsUrl="https://archive.org/download/nointro.gb/"
 archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.7z" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
