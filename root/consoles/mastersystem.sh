fcdb80edb95b422f05f9d574abce0bc10545692a
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-01-01 20:13:08 -0500

    Update mastersystem.sh

--- root/consoles/mastersystem.sh
+++ root/consoles/mastersystem.sh
@@ -3,10 +3,10 @@ consoleFolder="mastersystem"
 consoleRomFileExt=".bin, .sms, .zip, .7z"
 raConsoleId="11"
 uncompressRom="true"
-compressRom="true"
+compressRom="false"
 skyscraperPlatform="mastersystem"
 downloadAll="true"
 
 # Create URL List
-archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Sega%20-%20Master%20System%20-%20Mark%20III.zip/"
-archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
+archiveContentsUrl="https://archive.org/download/nointro.ms-mkiii/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.7z" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
