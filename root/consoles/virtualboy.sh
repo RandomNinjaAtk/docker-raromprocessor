b273c4af3ace828807b68431295e6926eb7bc201
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-01-01 20:10:11 -0500

    Update virtualboy.sh

--- root/consoles/virtualboy.sh
+++ root/consoles/virtualboy.sh
@@ -8,5 +8,5 @@ skyscraperPlatform="virtualboy"
 downloadAll="true"
 
 # Create URL List
-archiveContentsUrl="https://archive.org/download/hearto-1g1r-collection/hearto_1g1r_collection/Nintendo - Virtual Boy.zip/"
-archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep -i "archive.org" | sed 's%^//%https://%g' | sort -u)"
+archiveContentsUrl="https://archive.org/download/nointro.vb/"
+archiveUrl="$(wget -qO- "$archiveContentsUrl" | grep -i "\.7z" |  grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | sed 's/\///g' | sort -u | sed "s|^|$archiveContentsUrl|")"
