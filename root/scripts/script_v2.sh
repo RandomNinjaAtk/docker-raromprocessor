9b1bc1ceaa723bc9d50ef3f4adbf0166785fa2d5
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-01-04 10:51:13 -0500

    enable region selection

--- root/scripts/script_v2.sh
+++ root/scripts/script_v2.sh
@@ -5,7 +5,9 @@ scriptVersion="2"
 #raUsername=
 #raWebApiKey=
 libraryPath="/roms"
-consoles="psx,fds,pcfx,pcenginecd,fbneo,apple2,supervision,wasm4,megaduck,arduboy,channelf,atarist,c64,zxspectrum,x68000,pcengine,o2em,msx2,msx1,ngp,ngpc,amstradcpc,lynx,jaguar,atari2600,atari5200,vectrex,intellivision,wswan,wswanc,atari7800,colecovision,sg1000,virtualboy,pokemini,gamegear,gb,gbc,gba,nds,nes,snes,megadrive,mastersystem,sega32x,3do,n64,psp,segacd,saturn,psx,dreamcast,ps2"
+consoles="fds,pcfx,pcenginecd,fbneo,apple2,supervision,wasm4,megaduck,arduboy,channelf,atarist,c64,zxspectrum,x68000,pcengine,o2em,msx2,msx1,ngp,ngpc,amstradcpc,lynx,jaguar,atari2600,atari5200,vectrex,intellivision,wswan,wswanc,atari7800,colecovision,sg1000,virtualboy,pokemini,gamegear,gb,gbc,gba,nds,nes,snes,megadrive,mastersystem,sega32x,3do,n64,psp,segacd,saturn,psx,dreamcast,ps2"
+regions="usa,europe,world"
+#regions="usa,europe,world,japan,other"
 ParallelProcesses=10
 #consoles=psp
 ######### LOGGING
@@ -478,12 +480,11 @@ do
     continue
   fi
 
-  ProcessLinks USA "$archiveUrl"
-	ProcessLinks Europe "$archiveUrl"
-	ProcessLinks World "$archiveUrl"
-	ProcessLinks Japan "$archiveUrl"
-	ProcessLinks Other "$archiveUrl"
-  
+  IFS=',' read -r -a filters <<< "$regions"
+  for region in "${filters[@]}"
+  do
+    ProcessLinks $region "$archiveUrl"
+  done
   
 
   if [ -d  "$libraryPath/$consoleFolder" ]; then
