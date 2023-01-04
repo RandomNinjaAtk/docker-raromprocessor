2c64e4b7a4d0ada170ec89abea273754c3b5cc71
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-01-04 13:47:03 -0500

    Update script_v2.sh

--- root/scripts/script_v2.sh
+++ root/scripts/script_v2.sh
@@ -6,8 +6,8 @@ scriptVersion="2"
 #raWebApiKey=
 libraryPath="/roms"
 consoles="fds,pcfx,pcenginecd,fbneo,apple2,supervision,wasm4,megaduck,arduboy,channelf,atarist,c64,zxspectrum,x68000,pcengine,o2em,msx2,msx1,ngp,ngpc,amstradcpc,lynx,jaguar,atari2600,atari5200,vectrex,intellivision,wswan,wswanc,atari7800,colecovision,sg1000,virtualboy,pokemini,gamegear,gb,gbc,gba,nds,nes,snes,megadrive,mastersystem,sega32x,3do,n64,psp,segacd,saturn,psx,dreamcast,ps2"
-regions="usa,europe,world"
-#regions="usa,europe,world,japan,other"
+regions="usa,europe,world,hack,homebrew"
+#regions="usa,europe,world,japan,other,hack,homebrew"
 ParallelProcesses=10
 #consoles=psp
 ######### LOGGING
@@ -393,7 +393,7 @@ ProcessLinks () {
   regionArchiveUrl=$(cat "temp_url_list")
   rm temp_url_list
 	# Process ROMs with RAHasher
-	if [ "$region" = "Other" ]; then
+	if [ "$region" = "other" ]; then
 		region="."
 	fi
   N=$ParallelProcesses
